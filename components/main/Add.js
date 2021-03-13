9642d9ba310f6bd87b0c8d564cd5a9705c4ba6d8
Siddhant Kashyap <asksid27@gmail.com>
2021-03-13 21:29:05 +0530

    Camera and Gallery

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,10 +1,85 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { Camera } from "expo-camera";
+import * as ImagePicker from "expo-image-picker";
 
-export default function Add() {
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+
+      const pickerStatus = await ImagePicker.requestCameraPermissionsAsync();
+      setHasGalleryPermission(pickerStatus.status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  if (hasPermission === null || hasGalleryPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false || hasGalleryPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View>
-      <Text>Add</Text>
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Camera
+          style={styles.camera}
+          type={type}
+          ratio={"1:1"}
+          ref={(ref) => setCamera(ref)}
+        />
+      </View>
+      <Button
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      ></Button>
+      <Button title="Take Picture" onPress={() => takePicture()} />
+      <Button title="Take Image" onPress={() => pickImage()} />
+      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  camera: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+});
